5aedf7eeb5bd88c83cdc88629817d5e6a4f58e8a
Tmosy781 <tmosychukjr@gmail.com>
2024-10-16 18:28:32 -0400

    Created a new file background-styles.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import OrderPage from './pages/OrderPage';
 import OrderDetailPage from './pages/OrderDetailPage';
 import PrivateRoute from './components/PrivateRoute';
 import getUserInfo from './utilities/decodeJwt';
+import './background-styles.css'; // Updated import for the renamed CSS file
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -27,27 +28,29 @@ function App() {
 
   return (
     <CartProvider>
-      <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} isAdmin={checkIsAdmin()} />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage isAdmin={checkIsAdmin()} />} />
-        <Route path="/login" element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/order-confirmation" element={<OrderConfirmation />} />
-        {/* Protected Routes */}
-        <Route
-          path="/orders"
-          element={
-            <PrivateRoute element={OrderPage} isAuthenticated={isAuthenticated} />
-          }
-        />
-        <Route
-          path="/orders/:orderId"
-          element={
-            <PrivateRoute element={OrderDetailPage} isAuthenticated={isAuthenticated} />
-          }
-        />
-      </Routes>
+      <div className="min-h-screen pastel-rainbow-gradient">
+        <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} isAdmin={checkIsAdmin()} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage isAdmin={checkIsAdmin()} />} />
+          <Route path="/login" element={<LoginPage setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/order-confirmation" element={<OrderConfirmation />} />
+          {/* Protected Routes */}
+          <Route
+            path="/orders"
+            element={
+              <PrivateRoute element={OrderPage} isAuthenticated={isAuthenticated} />
+            }
+          />
+          <Route
+            path="/orders/:orderId"
+            element={
+              <PrivateRoute element={OrderDetailPage} isAuthenticated={isAuthenticated} />
+            }
+          />
+        </Routes>
+      </div>
     </CartProvider>
   );
 }
